33553172c7b5c348cc01109a76629f5d607b2f7c
georgi <matti.georgi@gmail.com>
2025-05-25 13:35:31 +0200

    API returns settings objects

--- src/nodetool/api/settings.py
+++ src/nodetool/api/settings.py
@@ -1,16 +1,16 @@
 from fastapi import APIRouter, HTTPException
 from nodetool.common.environment import Environment
 from nodetool.common.settings import load_settings, save_settings
+from nodetool.common.configuration import Setting, get_settings_registry
 from pydantic import BaseModel
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 
 router = APIRouter(prefix="/api/settings", tags=["settings"])
 
 
 class SettingsResponse(BaseModel):
-    settings: Dict[str, Any]
-    secrets: Dict[str, Any]
+    settings: List[Setting]
 
 
 class SettingsUpdateRequest(BaseModel):
@@ -25,15 +25,15 @@ async def get_settings() -> SettingsResponse:
             status_code=403, detail="Settings cannot be read in production"
         )
 
-    settings, secrets = load_settings()
+    settings = get_settings_registry()
 
-    return SettingsResponse(settings=settings, secrets=secrets)
+    return SettingsResponse(settings=settings)
 
 
 @router.put("/")
 async def update_settings(
     req: SettingsUpdateRequest,
-) -> SettingsResponse:
+) -> Dict[str, str]:
     if Environment.is_production():
         raise HTTPException(
             status_code=403, detail="Settings cannot be updated in production"
@@ -48,4 +48,4 @@ async def update_settings(
 
     Environment.load_settings()
 
-    return SettingsResponse(settings=settings, secrets=secrets)
+    return {"message": "Settings updated successfully"}
