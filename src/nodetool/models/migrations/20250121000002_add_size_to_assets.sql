fa1646fa20b6006750212cd3bc3b57d6b7f54b70
georgi <matti.georgi@gmail.com>
2025-07-21 00:26:48 +0200

    add migration

---
+++ src/nodetool/models/migrations/20250121000002_add_size_to_assets.sql
@@ -0,0 +1,4 @@
+-- Migration to add size column to assets table
+
+-- Add the size column for file size in bytes (NULL for folders)
+ALTER TABLE nodetool_assets ADD COLUMN IF NOT EXISTS size INTEGER; 
