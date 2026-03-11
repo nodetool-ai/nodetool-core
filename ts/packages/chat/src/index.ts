8aae03f4612de7b4f1d630e9d42096916d5b5310
georgi <matti.georgi@gmail.com>
2026-03-11 19:28:03 +0100

    remove typescript

--- ts/packages/chat/src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @nodetool/chat -- Chat processing with streaming and tool calling.
- */
-
-export { countTextTokens, countMessageTokens, countMessagesTokens } from "./token-counter.js";
-
-export { runTool, processChat } from "./message-processor.js";
-export type { ChatCallbacks } from "./message-processor.js";
--- ts/packages/protocol/src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @nodetool/protocol – Public API
- */
-
-export * from "./messages.js";
-export * from "./graph.js";
-export { TypeMetadata } from "./type-metadata.js";
-export { validateType, type ValidationResult } from "./typecheck.js";
-export { wrapPrimitive, unwrapPrimitive, type WrappedPrimitive } from "./wrap-primitives.js";
